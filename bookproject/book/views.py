2f10f66205c6b93c3cda8f7127a1f272784b383e
matamiya <matamiya_wsl2@DESKTOP-96BJS2O.localdomain>
2022-04-20 21:29:21 +0900

    5-5まで完了

--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,6 +1,7 @@
 from dataclasses import fields
 from re import template
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -36,3 +37,10 @@ class UpdateBookView(UpdateView):
     template_name = 'book/book_update.html'
     success_url = reverse_lazy('list-book')
 
+def index_view(request):
+    object_list = Book.objects.order_by('category')
+    return render(request,'book/index.html',{'object_list':object_list})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
