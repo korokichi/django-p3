b87d323e20a51f41e393cae014bc1b30cc4837d8
matamiya <matamiya_wsl2@DESKTOP-96BJS2O.localdomain>
2022-04-21 23:31:12 +0900

    5-8まで完了

--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,7 +1,7 @@
 from dataclasses import fields
 from re import template
 from django.shortcuts import render,redirect
-from django.urls import reverse_lazy
+from django.urls import reverse,reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -9,7 +9,7 @@ from django.views.generic import (
     DeleteView,
     UpdateView
     )
-from .models import Book
+from .models import Book,Review
 
 class ListBookView(ListView):
     template_name = 'book/book_list.html'
@@ -22,7 +22,7 @@ class DetailBookView(DetailView):
 class CreateBookView(CreateView):
     template_name = 'book/book_create.html'
     model = Book
-    fields = ('title','text','category')
+    fields = ('title','text','category','thumbnail')
     success_url = reverse_lazy('list-book')
 
 class DeleteBookView(DeleteView):
@@ -32,10 +32,28 @@ class DeleteBookView(DeleteView):
 
 class UpdateBookView(UpdateView):
     model = Book
-    fields = ('title','text','category')
+    fields = ('title','text','category','thumbnail')
     template_name = 'book/book_update.html'
     success_url = reverse_lazy('list-book')
 
 def index_view(request):
     object_list = Book.objects.order_by('category')
     return render(request,'book/index.html',{'object_list':object_list})
+
+class CreateReviewView(CreateView):
+    model = Review
+    fields = ('book','title','text','rate')
+    template_name = 'book/review_form.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['book'] = Book.objects.get(pk=self.kwargs['book_id'])
+        # print(context)
+        return context
+    
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('detail-book',kwargs={'pk':self.object.book.id})
--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -127,4 +130,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
-LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
